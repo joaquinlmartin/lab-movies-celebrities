87b68c13ce617974d168287c78509d739ca60d79
joaquinlmartin <joaquinlmartin@gmail.com>
2022-01-28 23:57:46 +0100

    iter 1 and 2 done, 3 coming

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,17 @@
+const { append } = require("express/lib/response");
+const Celebrity = require("../models/Celebrity.model");
+
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
 // all your routes here
+router.get('/celebrities/create', (req, res, next) => {
+
+    Celebrity.create()
+             .then((celebrity) => {
+                 res.render('celebrities/new-celebrity', {celebrity})
+             })
+             .catch(error => next (error));
+});
 
 module.exports = router;
